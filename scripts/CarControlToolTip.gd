6637291a867e139d6f44c7f6df2a69049555f963
Pro1d <topaffaire_pg@hotmail.fr>
2023-02-12 14:18:57 +0100

    Better car control tooltip

--- scripts/CarControlToolTip.gd
+++ scripts/CarControlToolTip.gd
@@ -1,6 +1,19 @@
 extends PanelContainer
 
-const actions := ["up", "down", "left", "right"]
+const IconUp := preload("res://resources/arrows/up.tres")
+const IconDown := preload("res://resources/arrows/down.tres")
+const IconLeft := preload("res://resources/arrows/left.tres")
+const IconRight := preload("res://resources/arrows/right.tres")
+const IconEnter := preload("res://resources/arrows/enter.tres")
+const icon_keys := {
+	KEY_UP: IconUp,
+	KEY_DOWN: IconDown,
+	KEY_LEFT: IconLeft,
+	KEY_RIGHT: IconRight,
+	KEY_ENTER: IconEnter,
+	KEY_KP_ENTER: IconEnter,
+}
+const actions := ["up", "down", "left", "right", "reset"]
 
 export var control_index := 0 setget set_control_index
 
@@ -10,10 +23,20 @@ onready var labels := [
 	$GridContainer/Down/Label,
 	$GridContainer/Left/Label,
 	$GridContainer/Right/Label,
+	$GridContainer/Reset/Label,
 ]
+onready var texture_rects := [
+	$GridContainer/Up/TextureRect,
+	$GridContainer/Down/TextureRect,
+	$GridContainer/Left/TextureRect,
+	$GridContainer/Right/TextureRect,
+	$GridContainer/Reset/TextureRect,
+]
+onready var init_offset := rect_position
 
 func _ready() -> void:
 	var _e := timer.connect("timeout", self, "on_timeout")
+	$AnimationPlayer.playback_speed = 1.0 / 0.4
 
 func set_player_color(player_id : int) -> void:
 	var style_box : StyleBoxFlat = get_stylebox("panel").duplicate()
@@ -30,9 +53,16 @@ func set_control_index(index : int) -> void:
 			if e is InputEventKey:
 				var key := e as InputEventKey
 				var local_scancode := OS.keyboard_get_scancode_from_physical(key.physical_scancode)
-				var s := OS.get_scancode_string(local_scancode)
-				labels[i].text = s.replace("Kp ", "")
-				print(action_prefix + actions[i], ": ", e.physical_scancode, " [", s, "]")
+				if local_scancode in icon_keys:
+					labels[i].visible = false
+					texture_rects[i].visible = true
+					(texture_rects[i] as TextureRect).texture = icon_keys[local_scancode]
+				else:
+					labels[i].visible = true
+					texture_rects[i].visible = false
+					var s := OS.get_scancode_string(local_scancode)
+					(labels[i] as Label).text = s.replace("Kp ", "")
+					print(action_prefix + actions[i], ": ", e.physical_scancode, " [", s, "]")
 				break # display only first
 			else:
 				print(action_prefix + actions[i], " not key event:", e)
